394714381187839c56fbef3d5dacc4a652c9276d
Shohei Maruyama <44642698+mtmaru@users.noreply.github.com>
2021-08-12 16:04:38 +0900

    コメントを追加。

--- likelihoods/binary.py
+++ likelihoods/binary.py
@@ -7,6 +7,9 @@ class Binary(PyroModule):
         super().__init__()
 
     def forward(self, f, y = None):
+        # f.shape = (num_output_dim, num_data_points)
+        # y.shape = (num_output_dim, num_data_points)
+
         # (num_output_dim, num_newdata_points)
         y = pyro.sample(self._pyro_get_fullname("y"), dist.Bernoulli(logits = f).to_event(f.dim()), obs = y)
 
